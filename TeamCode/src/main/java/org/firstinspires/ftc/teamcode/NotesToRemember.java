aee1ec414b80c9a7f0ea50677fbc1c51e3efb307
moekofi <43388005+moekofi@users.noreply.github.com>
2020-01-09 15:36:11 -0500

    Added notes, and color sensor exists now

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NotesToRemember.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+public class NotesToRemember {
+    /**
+
+
+    Notes for Autonomous: To avoid use of timer, instead make a variable in the opmode "stepDone" and set its condition to be true whenever the given task is completed.
+     Then, finish every step with
+
+     if (stepDone){
+        stepDone = false;
+        step++;
+        break;
+     }
+
+
+    */
+}
