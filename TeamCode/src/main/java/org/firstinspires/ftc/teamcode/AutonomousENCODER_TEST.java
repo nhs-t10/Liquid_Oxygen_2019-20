a1a874903b5c97e24a70daa975f0e8964389ca73
moekofi <43388005+moekofi@users.noreply.github.com>
2020-01-24 21:42:51 -0500

    Encoder rotation tests
    Next step of testing is setting power to 0.1f
    
    Testing values from there starting fresh at 570 (one full rotation)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousENCODER_TEST.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousENCODER_TEST.java
@@ -9,14 +9,45 @@ public class AutonomousENCODER_TEST extends Library {
     float [] omniValues = new float [4];
     int step = 1;
     ElapsedTime timer;
-    public boolean isYellow(){
-        return false;
+
+    public void delay(double delay){
+        double endTime = timer.milliseconds()+delay;
+        while(timer.milliseconds() <= endTime){
+            //Be patient
+        }
     }
+
     public void modeBreak(){
         lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+    public void encodeStrafe(float power, int distance){
+        lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lf.setTargetPosition(distance);
+        lb.setTargetPosition(-distance);
+        rf.setTargetPosition(-distance);
+        rb.setTargetPosition(distance);
+        lb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        drive(power, power, power, power);
+        while (Math.abs(lf.getCurrentPosition()) < Math.abs(distance) /**&& rb.getCurrentPosition() < distance*/){
+            telemetry.addData("lf Busy?", lf.isBusy());
+            telemetry.addData("rb Busy?", rb.isBusy());
+            telemetry.addData("Porgress", lf.getCurrentPosition());
+            telemetry.addData("Progress", rb.getCurrentPosition());
+            telemetry.update();
+            //Wait patiently
+        }
+        modeBreak();
+        drive(0,0,0,0);
+
     }
     public void encodeRotate(float power, int distance){
         lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -31,8 +62,8 @@ public class AutonomousENCODER_TEST extends Library {
         lf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        drive(power, power, power, power);
-        while (lf.getCurrentPosition() < distance /**&& rb.getCurrentPosition() < distance*/){
+        drive(-power, -power, power, power);
+        while (Math.abs(lf.getCurrentPosition()) < Math.abs(distance) /**&& rb.getCurrentPosition() < distance*/){
             telemetry.addData("lf Busy?", lf.isBusy());
             telemetry.addData("rb Busy?", rb.isBusy());
             telemetry.addData("Porgress", lf.getCurrentPosition());
@@ -67,7 +98,7 @@ public class AutonomousENCODER_TEST extends Library {
             //Wait patiently
         }
         modeBreak();
-        drive(0,0,0,0);
+        drive(0, 0, 0, 0);
 
     }
     public void init() {
@@ -80,8 +111,8 @@ public class AutonomousENCODER_TEST extends Library {
                 step++;
                 break;
             case (2):
-                encodeLinear(0.6f, 1000);
-
+                //encodeLinear(0.3f, 1000 );
+                encodeRotate(0.2f, 350);
                 step++;
                 break;
         }
